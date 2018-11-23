f9914c20746e804046e11dd599464be3e7b1a966
Gökhan Sarı <gokhan@gokhansari.me>
2018-11-23 23:10:16 +0300

    added docs.

---
+++ docs/4-dfa497505fe8236ce2c9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{126:function(n){n.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
